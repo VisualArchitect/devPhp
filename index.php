5563b10ecfc5b6f73abb91e083c302ae4573312f
Ricardo Sewell <ricardosewell@Ricardos-MacBook-Pro.local>
2021-11-01 15:29:45 -0400

    test

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Developer News</h1>
+
+<?php 
+
+
+// variable
+$name = "Jeff";
+
+// array
+$colors = array("Magenta", "Yellow", "Cyan");
+
+// boolean
+$x = true;
+
+// class
+class Car {
+  function Car() {
+      $this->model = "Tesla";
+  }
+}
+
+// create an object
+$Lightning = new Car();
+
+echo "The Best PHP Examples"; 
+echo "<p> " . $name . " is best!"; ?>
+
+
+</body>
+</html> 
